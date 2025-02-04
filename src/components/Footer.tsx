14a3874e491acd20d895d459aa7fc6922a8eef70
Bautista Pietraroia <bautistapietraroia@gmail.com>
2025-02-04 15:51:31 -0300

    Fix footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,7 +19,7 @@ export const Footer = () => {
         <p className='text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4'>
           © 2024 La Delicia —
           <Link
-            href='https://wetby.com.ar'
+            href='https://webty.com.ar'
             className='text-gray-600 ml-1'
             rel='noopener noreferrer'
             target='_blank'>
